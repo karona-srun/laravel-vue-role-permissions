b853932c63b81b99d6e9e3c61cfedd9fda6adfe0
Karona Srun <karonasrun.ks@gmail.com>
2021-10-25 11:50:22 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('{any}', function () {
+    return view('layouts.vue');
+})->where('any', '.*');
+
+Route::post('social/{provider}', 'AuthController@social');
+
 
 Route::get('/', function () {
     return view('welcome');
